3e7516602352dc2073fe1644d2ce588f6981568d
Beverley Sun <beverleysun0205@gmail.com>
2020-05-29 23:00:59 +1200

    Get all .java files in a directory

--- src/visibilitychecker/Main.java
+++ src/visibilitychecker/Main.java
@@ -1,4 +1,22 @@
 package visibilitychecker;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args){
+
+        // Input from user
+        System.out.println("Input directory path of .java files:");
+        Scanner scan = new Scanner(System.in);
+        String folderName = scan.nextLine();
+
+        // Get file names in the folder specified by user
+        Directory folder = new Directory(folderName);
+        List<String> fileNames = folder.getFileNames();
+
+        // Check .java files
+        Checker checker = new Checker(fileNames,folderName);
+    }
 }
