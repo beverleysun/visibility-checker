953bdb53df3d395ed1b0e4f9da5a9e547b6c6268
Beverley Sun <beverleysun0205@gmail.com>
2020-05-30 19:55:34 +1200

    Get all methods and fields in a class

---
+++ src/visibilitychecker/Checker.java
@@ -0,0 +1,45 @@
+package visibilitychecker;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class Checker {
+    private String _packageName;
+    private List<String> _classNames;
+
+    public Checker(String packageName, List<String> classNames) {
+        _packageName = packageName;
+        _classNames = classNames;
+    }
+
+    public void check(){
+        Class cls;
+        Field[] fields;
+        Method[] methods;
+
+        for (String className: _classNames) {
+            System.out.println("For" + className + ":");
+            try {
+                cls = Class.forName(_packageName + "." + className);
+                fields = cls.getFields();
+                methods = cls.getMethods();
+
+
+                System.out.println("Fields:");
+                for (Field field: fields) {
+                    System.out.println(field);
+                }
+
+                System.out.println("Methods:");
+                for (Method method: methods) {
+                    System.out.println(method);
+                }
+            } catch (ClassNotFoundException e) {
+                System.out.println(e.toString());
+            }
+            System.out.println();
+        }
+    }
+
+}
