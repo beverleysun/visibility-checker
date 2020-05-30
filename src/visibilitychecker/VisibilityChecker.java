4dbe225e9c4d9c726833a26ec8bb6ff59b95b77b
Beverley Sun <beverleysun0205@gmail.com>
2020-05-30 19:53:29 +1200

    Completed functionality

---
+++ src/visibilitychecker/VisibilityChecker.java
@@ -0,0 +1,21 @@
+package visibilitychecker;
+
+import java.util.List;
+
+public class VisibilityChecker {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        // Get package name of java files for checking
+        PackageName packageName = new PackageName();
+        String packageForChecking = packageName.getPackageForChecking();
+
+        // Get all class names to check
+        List<String> classNames = new Directory(packageForChecking).getClassNames();
+        System.out.println(classNames);
+
+        // Check all classes
+        Checker checker = new Checker(packageForChecking, classNames);
+        checker.check();
+    }
+}
