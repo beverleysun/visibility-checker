e4b4d990352b5e9af04aa3e9fe95c9bf866e29a6
Beverley Sun <beverleysun0205@gmail.com>
2020-07-27 18:25:25 +1200

    Open a window

--- src/visibilitychecker/VisibilityChecker.java
+++ src/visibilitychecker/VisibilityChecker.java
@@ -1,21 +1,30 @@
 package visibilitychecker;
 
+import javax.swing.*;
 import java.util.List;
 
-public class VisibilityChecker {
+public class VisibilityChecker extends JFrame {
 
     public static void main(String[] args) {
+        VisibilityChecker visChecker = new VisibilityChecker();
+        visChecker.drawWindow();
 
-        // Get package name of java files for checking
-        PackageName packageName = new PackageName();
-        String packageForChecking = packageName.getPackageForChecking();
+//        // Get package name of java files for checking
+//        PackageName packageName = new PackageName();
+//        String packageForChecking = packageName.getPackageForChecking();
+//
+//        // Get all class names to check
+//        List<String> classNames = new Directory(packageForChecking).getClassNames();
+//        System.out.println(classNames);
+//
+//        // Check all classes
+//        Checker checker = new Checker(packageForChecking, classNames);
+//        checker.check();
+    }
 
-        // Get all class names to check
-        List<String> classNames = new Directory(packageForChecking).getClassNames();
-        System.out.println(classNames);
-
-        // Check all classes
-        Checker checker = new Checker(packageForChecking, classNames);
-        checker.check();
+    public void drawWindow() {
+        setSize(400,400);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 }
