f2a969af053462d55041c99cd4f768886767a0c6
Beverley Sun <beverleysun0205@gmail.com>
2020-07-27 21:49:51 +1200

    Added output to text area on GUI

--- src/visibilitychecker/VisibilityChecker.java
+++ src/visibilitychecker/VisibilityChecker.java
@@ -1,11 +1,6 @@
 package visibilitychecker;
 
-import javax.swing.JFrame;
-import javax.swing.JCheckBox;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
+import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -14,8 +9,13 @@ import java.util.List;
 
 public class VisibilityChecker extends JFrame {
 
+    private int _windowSizeWidth = 600;
+    private int _windowSizeHeight = 400;
+    private String _title = "Visibility Checker";
+
     private JCheckBox _publicVisBox, _privateVisBox, _protectedVisBox, _packageVisBox;
     private JCheckBox _methodsBox, _fieldsBox;
+    private JTextArea _resultTextArea;
     private boolean _publicVisSelected, _privateVisSelected, _protectedVisSelected, _packageVisSelected;
     private boolean _methodsSelected, _fieldsSelected;
 
@@ -33,42 +33,54 @@ public class VisibilityChecker extends JFrame {
 
         // Get all class names to check
         List<String> classNames = new Directory(packageForChecking).getClassNames();
-        System.out.println("Classes to check: ");
+
+        _resultTextArea.append("Classes to check: " + "\n");
         for (String className : classNames){
-            System.out.println("    " + className);
+            _resultTextArea.append("        " + className + "\n");
         }
-        System.out.println();
+        _resultTextArea.append("\n");
 
         // Check all classes
         Checker checker = new Checker(packageForChecking, classNames);
-        checker.check(_publicVisSelected, _privateVisSelected, _protectedVisSelected, _packageVisSelected);
-
+        checker.check(_publicVisSelected, _privateVisSelected, _protectedVisSelected, _packageVisSelected, _fieldsSelected, _methodsSelected, _resultTextArea);
     }
 
     public void drawWindow() {
-        setSize(600,400);
+        setSize(_windowSizeWidth, _windowSizeHeight);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("Visibility Checker");
+        setTitle(_title);
         setVisible(true);
+        setResizable(false);
     }
 
     public void setUpGUI() {
+        // Initialise panels
         JPanel mainPanel = new JPanel();
         JPanel operationPanel = new JPanel();
         JPanel visibilityPanel = new JPanel();
         JPanel methodsFieldsPanel = new JPanel();
+        JPanel executePanel = new JPanel();
+        JPanel resultPanel = new JPanel();
 
-        operationPanel.setLayout(new GridLayout());
-
+        // Initialise borders for panels
         Border operationBorder = BorderFactory.createTitledBorder("Visibility To Check For:");
         Border methodsFieldsBorder = BorderFactory.createTitledBorder("Methods and Fields:");
+        Border resultBorder = BorderFactory.createTitledBorder("Result:");
 
         visibilityPanel.setBorder(operationBorder);
         visibilityPanel.setLayout(new BoxLayout(visibilityPanel, BoxLayout.PAGE_AXIS));
 
         methodsFieldsPanel.setBorder(methodsFieldsBorder);
         methodsFieldsPanel.setLayout(new BoxLayout(methodsFieldsPanel, BoxLayout.PAGE_AXIS));
+
+        resultPanel.setBorder(resultBorder);
+        resultPanel.setLayout(new BoxLayout(resultPanel, BoxLayout.PAGE_AXIS));
+
+        // Create components
+        _resultTextArea = new JTextArea();
+        _resultTextArea.setEditable(false);
+        JScrollPane scroll = new JScrollPane(_resultTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 
         _fieldsBox = new JCheckBox("Methods");
         _methodsBox = new JCheckBox("Fields");
@@ -81,6 +93,13 @@ public class VisibilityChecker extends JFrame {
         JButton executeButton = new JButton("Run");
         executeButton.addActionListener(new ButtonListener());
 
+        // Setting layouts
+        operationPanel.setLayout(new GridLayout());
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+        operationPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 11));
+        executePanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 10));
+
+        // Adding components to panels
         methodsFieldsPanel.add(_fieldsBox);
         methodsFieldsPanel.add(_methodsBox);
 
@@ -91,18 +110,32 @@ public class VisibilityChecker extends JFrame {
 
         operationPanel.add(methodsFieldsPanel);
         operationPanel.add(visibilityPanel);
+        executePanel.add(executeButton);
+
+        resultPanel.add(scroll);
+
         mainPanel.add(operationPanel);
-        mainPanel.add(executeButton);
+        mainPanel.add(executePanel);
+        mainPanel.add(resultPanel);
+
         add(mainPanel);
     }
 
     private class ButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+            _fieldsSelected = _fieldsBox.isSelected();
+            _methodsSelected = _methodsBox.isSelected();
+
             _publicVisSelected = _publicVisBox.isSelected();
             _privateVisSelected = _privateVisBox.isSelected();
             _protectedVisSelected = _protectedVisBox.isSelected();
             _packageVisSelected = _packageVisBox.isSelected();
+
+            //reset text
+            _resultTextArea.setText(null);
+
             execute();
+            _resultTextArea.update(_resultTextArea.getGraphics());
         }
     }
 }
